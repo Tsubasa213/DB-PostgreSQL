d02dee598e1ddd6751894f9a808218a9d4263ff8
Sharla <116696387+Tsubasa213@users.noreply.github.com>
2025-10-30 11:01:47 +0900

    第4回のドリル

---
+++ sql/04/drill/ex-02_1.sql
@@ -0,0 +1,10 @@
+SELECT
+    id,
+    name,
+    level,
+    guild
+FROM
+    s_characters
+ORDER BY
+    guild NULLS FIRST,
+    level ;
---
+++ sql/04/tmp.sql
@@ -0,0 +1,17 @@
+SELECT
+    id,
+    name,
+    job,
+    level
+FROM
+    s_characters
+ORDER BY
+    CASE
+        WHEN job = 'Priest' OR
+        job = 'Wizard' THEN 1
+        ELSE 2
+    END ASC, -- CASE式による第1キー
+    job, -- 第2キー
+    level DESC;
+
+-- 第3キー
